ead3ec31a9c2f15754de591eafe1edc1605f84f2
jwaoyo333 <165109274+jwaoyo333@users.noreply.github.com>
2024-04-16 21:14:36 +0900

    Create 2.c

---
+++ Mid-term/2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    scanf("%d", &a);
+    for(int i = 0 ; i< a; i++){
+        for(int j = a-i-1 ; j>0;j-- ){
+            printf("0");
+        }
+        for(int j = 0 ; j<i+1 ; j++){
+            printf("%c", i+65);
+        }
+        printf("\n");
+    }
+    int b = a-1;
+    for(int i = b ; i>0; i--){
+        for(int j = 0 ; j<a-i ; j++){
+            printf("0");
+        }
+        for(int j = i ; j>0 ; j--){
+            printf("%c", i+64);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}

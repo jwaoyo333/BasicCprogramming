cf31cad4a70bacbfb5dbe689df45b7fb2a003343
jwaoyo333 <165109274+jwaoyo333@users.noreply.github.com>
2024-04-30 14:29:26 +0900

    Update lab1.c

--- 0430/lab1.c
+++ 0430/lab1.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 
 int main(void){
+    char c = '0';
+    char *pc = &c;
+    int m = 100;
+    int *pm = &m;
+    double x = 5.83;
+    double *px = &x;
 
-    double x,y;
-    double* px = &x;
-    double* py = &y;
-
-    printf("두 실수 입력 : ");
-    scanf("%lf %lf", px, py);
-
-    printf("%.2f + %.2f = %.2f\n", *px,*py,*px+*py);
-    
+    printf("변수명  주소값          저장값\n");
+    printf("------------------------------\n");
+    printf("%3s %12p %9c\n", "c", pc,c);
+    printf("%3s %12p %9d\n", "m", pm,m);
+    printf("%3s %12p %9f\n", "x", px,x);
     return 0;
 }

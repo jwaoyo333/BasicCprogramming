03fc96a590f599fd69ebdf9bc621a5f8092173ec
jwaoyo333 <165109274+jwaoyo333@users.noreply.github.com>
2024-04-30 13:50:57 +0900

    Create 7.c

---
+++ 0430/7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+
+    int i =100;
+    int *pi = &i;
+    int **dpi = &pi;
+
+    printf("%p %p %p\n", &i,pi,*dpi);
+
+    *pi = i+30;
+
+    printf("%d %d %d\n", i,*pi,**dpi);
+
+    **dpi = *pi + 30 ;
+    printf("%d %d %d\n", i,*pi,**dpi);
+
+    return 0;
+}
